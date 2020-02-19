d76427d6f3e7dcb5404fcd5399ec53412bdaf92e
tvaidotas <tadas.vaidotas@gmail.com>
2020-02-19 15:12:58 +0000

    adding placeholder Actions for the routes updated the controller name in the routes file

--- app/controllers/CDController.scala
+++ app/controllers/CDController.scala
@@ -1,11 +1,18 @@
 package controllers
 
 import javax.inject._
+import models.CD
 import play.api.mvc._
 
 @Singleton
 class CDController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport {
 
+  def showCDForm() = Action { implicit request: Request[AnyContent] =>
+    Ok("Nothing to see yet")
+  }
 
+  def submitCD = Action { implicit request: Request[AnyContent] =>
+    Ok("Nothing to return yet")
+  }
 
 }
